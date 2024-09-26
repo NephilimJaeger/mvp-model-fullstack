df66369ec195a5538793d69515becafc832e9e26
Nilane Arruda <nyllane16@gmail.com>
2024-09-25 21:05:53 -0300

    Novo schema para a tabela no banco de dados

--- app/database/db_models.py
+++ app/database/db_models.py
@@ -5,27 +5,27 @@ from dataclasses import dataclass
 
 Base = declarative_base()
 
+
 @dataclass
 class NearestEarthObjectInfo:
     name: str
-    est_diamenter_min: int
     est_diameter_max: int
     relative_velocity: int
+    absolute_magnitude: float
+
 
 class NearestEarthObject(Base):
     __tablename__ = "nearest_earth_object"
 
-    id = Column(Integer, primary_key=True,autoincrement=True)
-    name = Column(String, primary_key=True, unique= True)
-    est_diamenter_min = Column(Float)
+    name = Column(String, primary_key=True, unique=True)
     est_diameter_max = Column(Float)
     relative_velocity = Column(Float)
+    absolute_magnitude = Column(Float)
     hazardous = Column(Boolean)
-
 
     def __init__(self, nearest_earth_object: NearestEarthObjectInfo, hazardous: bool):
         self.name = nearest_earth_object.name
-        self.est_diamenter_min = nearest_earth_object.est_diamenter_min
         self.est_diameter_max = nearest_earth_object.est_diameter_max
         self.relative_velocity = nearest_earth_object.relative_velocity
+        self.absolute_magnitude = nearest_earth_object.absolute_magnitude
         self.hazardous = hazardous
