da98dbc918899b4cb9c6b01438262970c3634c45
Nilane Arruda <nyllane16@gmail.com>
2024-09-25 21:04:39 -0300

    Ajustes na funçao de predição para se adaptar ao novo schema

--- app/ml_predict.py
+++ app/ml_predict.py
@@ -4,10 +4,10 @@ from app.database.db_models import NearestEarthObjectInfo
 
 def predict_hazardous(neo_info: NearestEarthObjectInfo) -> bool:
     """Realiza a predição de periculosidade de um objeto celeste."""
-    
-    with open('data/pipeline.pkl', 'rb') as file:
+
+    with open("data/pipeline.pkl", "rb") as file:
         pipeline = load(file)
-    data = [[neo_info.est_diamenter_min, neo_info.relative_velocity]]
+    data = [[neo_info.est_diameter_max, neo_info.relative_velocity, neo_info.absolute_magnitude]]
     prediction = pipeline.predict(data)
 
     return bool(prediction)
