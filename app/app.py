c86d06914ec0e5269470c741c0596f1bd2ef54a5
Nilane Arruda <nyllane16@gmail.com>
2024-09-25 21:05:34 -0300

    Dosctrings,black formatter e novo schema para a tabela de predições

--- app/app.py
+++ app/app.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI, HTTPException
-from app.schemas import ErrorSchema, NearestEarthObjectInfo, NearestEarthObjectInfoResponse
+from app.schemas import (
+    ErrorSchema,
+    NearestEarthObjectInfo,
+    NearestEarthObjectInfoResponse,
+)
 from app.database import session
 from app.ml_predict import predict_hazardous
 
@@ -10,12 +14,21 @@ app = FastAPI()
     400: {"description": "Error Response", "model": ErrorSchema}
 })
 def predict_nearest_earth_object(neo_info: NearestEarthObjectInfo):
+    """
+    Endpoint para prever se um objeto próximo à Terra (NEO) é perigoso.
+    Args:
+        neo_info (NearestEarthObjectInfo): Informações sobre o objeto.
+            - name (str): Nome.
+            - est_diameter_max (float): Diâmetro estimado máximo, em quilômetros.
+            - relative_velocity (float): Velocidade relativa em relação à Terra em km/h.
+            - absolute_magnitude (float): Luminosidade.
+    """
     try:
         hazardous = predict_hazardous(neo_info)
         neo_info.insert_neo_to_db(session, hazardous)
         response = NearestEarthObjectInfoResponse.from_neo_info(neo_info, hazardous)
         return response
     except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Erro ao inserir o objeto no banco de dados: {e}")
-
-
+        raise HTTPException(
+            status_code=400, detail=f"Erro ao inserir o objeto no banco de dados: {e}"
+        )
--- app/schemas.py
+++ app/schemas.py
@@ -2,27 +2,45 @@ from pydantic import BaseModel
 from sqlalchemy.orm.session import Session
 from app.database.db_models import NearestEarthObject
 
+
 class ErrorSchema(BaseModel):
     """Define como uma mensagem de erro deve ser retornada."""
 
     mensagem: str
 
+
 class NearestEarthObjectInfo(BaseModel):
+    """
+    Classe que representa as informações de um objeto próximo à Terra (NEO).
+
+    Atributos:
+        name (str): Nome do objeto.
+        est_diameter_max (float): Diâmetro estimado máximo do objeto em quilômetros.
+        relative_velocity (float): Velocidade relativa do objeto em relação à Terra em km/h.
+        absolute_magnitude (float): Magnitude absoluta do objeto, que é uma medida de sua luminosidade.
+    """
+
     name: str
-    est_diamenter_min: float
     est_diameter_max: float
     relative_velocity: float
+    absolute_magnitude: float
 
     def check_if_neo_exists(self, session: Session) -> bool:
         """Verifica se um objeto próximo à Terra já existe no banco de dados."""
-        neo = session.query(NearestEarthObject).filter(NearestEarthObject.name == self.name).first()
+        neo = (
+            session.query(NearestEarthObject)
+            .filter(NearestEarthObject.name == self.name)
+            .first()
+        )
         return neo is not None
 
     def insert_neo_to_db(self, session: Session, hazardous: bool) -> NearestEarthObject:
         """Insere um objeto próximo à Terra no banco de dados."""
         try:
             if self.check_if_neo_exists(session):
-                raise ValueError("O objeto próximo à Terra já existe no banco de dados.") 
+                raise ValueError(
+                    "O objeto próximo à Terra já existe no banco de dados."
+                )
             neo = NearestEarthObject(nearest_earth_object=self, hazardous=hazardous)
             session.add(neo)
             session.commit()
@@ -33,20 +51,18 @@ class NearestEarthObjectInfo(BaseModel):
             raise e
         finally:
             session.close()
-        
-       
+
+
 class NearestEarthObjectInfoResponse(BaseModel):
-    """Define como um objeto de informação de um objeto próximo à Terra deve ser retornado."""
+    """Define os campos retornados pela API."""
+
     name: str
     message: str
     hazardous: bool
 
     @classmethod
-    def from_neo_info(cls, neo_info: NearestEarthObjectInfo, hazardous: bool) -> "NearestEarthObjectInfoResponse":
-        """Cria a resposta que será retornada na API."""
+    def from_neo_info(
+        cls, neo_info: NearestEarthObjectInfo, hazardous: bool
+    ) -> "NearestEarthObjectInfoResponse":
         message = f"O objeto {neo_info.name} foi inserido em nossa base de dados com sucesso. Periculosidade: {'Sim' if hazardous else 'Não'}."
         return cls(name=neo_info.name, message=message, hazardous=hazardous)
-
-
-
-    
